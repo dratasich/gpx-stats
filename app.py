4c03fe599fbf0a635aba7ae05c47ed8218d1cf5a
Denise Ratasich <dratasich@users.noreply.github.com>
2022-11-20 16:03:36 +0100

    Read db data into dash app

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 # %% load
+import logging
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from datetime import datetime
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+from sqlalchemy import create_engine
 
 
-# %% sample data
-df = pd.DataFrame([
-        {"date": datetime.strptime("2020-01-05", "%Y-%m-%d"), "speed": 3.1},
-        {"date": datetime.strptime("2020-02-05", "%Y-%m-%d"), "speed": 4.0},
-        {"date": datetime.strptime("2020-02-09", "%Y-%m-%d"), "speed": 3.5},
-        {"date": datetime.strptime("2020-03-05", "%Y-%m-%d"), "speed": 3.3},
-        {"date": datetime.strptime("2020-05-05", "%Y-%m-%d"), "speed": 3.0},
-    ])
+# %% init logger
+logging.basicConfig(level=logging.INFO,
+                    format="[%(asctime)s] [%(levelname)s] %(message)s")
+
+# %% database setup
+DB = "gpx.db"
+logging.info(f"Connect to '{DB}'...")
+engine = create_engine(f"sqlite:///{DB}")
+TABLE = "summary"
+df = pd.read_sql(f"SELECT * from {TABLE}", engine)
+df = df.astype({"date": "datetime64"})
+logging.info(f"Loaded '{len(df)}' entries from table {TABLE}")
+df.info()
+print(df.head())
 
 # add columns for grouping
 df["year"] = df.apply(lambda row: row.date.year, axis=1)
@@ -23,9 +30,9 @@ df["month"] = df.apply(lambda row: row.date.month, axis=1)
 df["week"] = df.apply(lambda row: row.date.week, axis=1)
 
 # speed values to standard-running units (km/h)
-df["speed"] = df["speed"] * 3.6
+df["speed (km/h)"] = df["speedMetersPerSecond"] * 3.6
 # add pace min/km (from speed in m/s)
-df["pace"] = 60 / df["speed"]
+df["pace (min/km)"] = 60 / df["speed (km/h)"]
 
 # %% plot
 #fig = px.box(df, x="month", y="speed", points="all")
@@ -50,8 +57,8 @@ app.layout = html.Div([
     dcc.RadioItems(
         id='y-axis',
         options=[{'value': x, 'label': x}
-                 for x in ['speed', 'pace']],
-        value='speed',
+                 for x in ['speed (km/h)', 'pace (min/km)']],
+        value='speed (km/h)',
         labelStyle={'display': 'inline-block'}
     ),
     dcc.Graph(id="box-plot"),
@@ -63,7 +70,7 @@ app.layout = html.Div([
     Input("y-axis", "value"),
 )
 def box_plot(x, y):
-    fig = px.box(df, x=x, y=y, points="all")
+    fig = px.box(df, x=x, y=y, points="all", color="activity")
     return fig
 
 app.run_server(debug=True)
